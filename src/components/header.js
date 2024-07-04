3f4b7f14c04e8621f46f9356143432dfdaaea0af
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-07-04 15:27:36 +0200

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const header = ({ toogleModal, nbOfContacts }) => {
+const header = ({ toggleModal, nbOfContacts }) => {
   return (
     <header className='header'>
         <div className='container'>
             <h3>Contact List({nbOfContacts})</h3>
-            <button onClick={() => toogleModal(true)} className='btn'>
+            <button onClick={() => toggleModal(true)} className='btn'>
                 <i className='bi bi-plus-quare'></i>Add New Contact
                 </button>
         </div>
