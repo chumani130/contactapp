06b474183a5c12637541f5c9de65a697de64b858
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-07-09 15:21:02 +0200

    Update contactList.js

--- src/components/contactList.js
+++ src/components/contactList.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import Contact from "./Contact"
-import { Link } from 'react-router-dom';
 
-const contactList = ({data, currentPage, getAllContacts }) => {
+const ContactList = ({data, currentPage, getAllContacts }) => {
   return (
     <main className='main'>
         {data?.content?.length === 0 && <div>No Contacts. Please add a new contact</div>}
 
-       <ul className='contact_list'>
-            {data?.content?.length > 0 && data.content.map(contact => <Contact contact={contact} key={contact.id} />)}
+       <ul className='contact__list'>
+            {data?.content?.length > 0 && data.content.map(contact => 
+            <Contact contact={contact} key={contact.id} />)}
        </ul>
 
        {data?.content?.length > 0 && data?.totalPages > 1 && 
        <div className='pagination'>
-        <Link onClick={() => getAllContacts(currentPage - 1)} className={0 === currentPage ? 'disabled' : ''}>
-            &laquo;</Link>
-        data && {[...Array(data.totalPages).keys()].map((page, index) => 
-        <Link onClick={getAllContacts(page)} className={currentPage === page ? 'active' : ''} key={page}>{page +1}</Link>)}
-        <Link onClick={() => getAllContacts(currentPage + 1)} className={data.totalPages === currentPage ? 'disabled' : ''}>
-            &raquo;</Link>
+        <a onClick={() => getAllContacts(currentPage - 1)} className={0 === currentPage ? 'disabled' : ''}> &laquo;</a>
+
+        { data && [...Array(data.totalPages).keys()].map((page, index) => 
+        <a onClick={() => getAllContacts(page)} className={currentPage === page ? 'active' : ''} key={page}>{page +1}</a>)}
+
+        <a onClick={() => getAllContacts(currentPage + 1)} className={data.totalPages === currentPage ? 'disabled' : ''}>
+            &raquo;</a>
 
        </div> 
        }
@@ -26,4 +27,4 @@ const contactList = ({data, currentPage, getAllContacts }) => {
   )
 }
 
-export default contactList
+export default ContactList
