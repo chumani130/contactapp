332b822fb80a766b782b8d68c04e6047d3b3d40b
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-07-09 15:20:55 +0200

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const contact = ({ contact }) => {
+const Contact = ({ contact }) => {
   return (
     <Link to={`/contacts/${contact.id}`} className="contact__item">
-        <div className='contact_header'>
-            <div className='contact_image'>
+        <div className='contact__header'>
+            <div className='contact__image'>
                 <img src={contact.photoUrl} alt={contact.name} />
             </div>
-            <div className='contact_details'>
+            <div className='contact__details'>
                 <p className='contact_name'>{contact.name.substring(0, 15)}</p>  
                 {/* we do not want the name to be too long, so we set it to 0 and 15 characters */}
                 <p className='contact_title'>{contact.title}</p>
             </div>
         </div>
-        <div className='contact_body'>
-            <p><i className='bi bi-envelope'></i> {contact.email.substring(0, 15)}</p>
+        <div className='contact__body'>
+            <p><i className='bi bi-envelope'></i> {contact.email.substring(0, 20)}</p>
             <p><i className='bi bi-geo'></i> {contact.address}</p>
             <p><i className='bi bi-telephone'></i> {contact.phone}</p>
             <p>{contact.status === 'Active' ? <i className='bi bi-check-circle'></i> :
@@ -25,4 +25,4 @@ const contact = ({ contact }) => {
   )
 }
 
-export default contact
+export default Contact
